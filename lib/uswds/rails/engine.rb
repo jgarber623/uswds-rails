a02f5e1e1734d1993c9a699de33163baea7dcccf
Jason Garber <jason@sixtwothree.org>
2017-07-19 22:14:05 -0400

    Add assets to precompile list

--- lib/uswds/rails/engine.rb
+++ lib/uswds/rails/engine.rb
@@ -1,3 +1,7 @@
+require 'bourbon'
+require 'neat'
+require 'normalize-scss'
+
 module Uswds
   module Rails
     class Engine < ::Rails::Engine
@@ -5,6 +9,8 @@ module Uswds
         %w[fonts images javascripts stylesheets].each do |path|
           app.config.assets.paths << root.join('vendor', path).to_s
         end
+
+        app.config.assets.precompile += [/uswds\/.+\.(eot|png|svg|ttf|woff|woff2)$/]
       end
     end
   end
